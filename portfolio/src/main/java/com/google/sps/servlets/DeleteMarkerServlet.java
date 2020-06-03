50a52dc8f4fdf92d4620967a3331d4f0af88ade9
Nikhil Mittu <nick@mittudev.com>
2020-06-03 14:53:00 +0000

    Remove unused imports.

--- portfolio/src/main/java/com/google/sps/servlets/DeleteMarkerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteMarkerServlet.java
@@ -16,14 +16,8 @@ package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.google.gson.Gson;
-import com.google.sps.comments.Comment;
-import java.util.ArrayList;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
